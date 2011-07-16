9fe74096335237e242dd46f1aedeccb54bd55d41
Mattt Thompson <m@mattt.me>
2011-07-16 09:16:11 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inquistr::Application.config.secret_token = '17e254210e18d2680cf2f27be491a64407ce7dcb4d05863000ea9166a819507549669124a4ab21c99c0e48259f7b649b50c69df09c1b0f1e7a0cc073073b3ada'
